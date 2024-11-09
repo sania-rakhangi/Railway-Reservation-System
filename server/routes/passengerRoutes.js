e9374d1b17c7eed93579e096a2533d06afe9ba5e
sania-rakhangi <147305583+sania-rakhangi@users.noreply.github.com>
2024-11-09 14:59:26 +0530

    deleted unused files

--- server/routes/passengerRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { searchTrains } = require("../controllers/passengerController");
-const router = express.Router();
-
-router.post("/search", searchTrains);
-
-module.exports = router;
